1dd15b45b2004f33a98503529dae7165f13c1e05
deadlord082 <135032460+deadlord082@users.noreply.github.com>
2025-07-10 16:03:24 +0200

    initial commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,35 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Express' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'Express' });
+});
+
+router.get('/antennes', function(req, res, next) {
+  res.render('antennes', { title: 'Express' });
+});
+
+router.get('/preventions', function(req, res, next) {
+  res.render('preventions', { title: 'Express' });
+});
+
+router.get('/france_avc', function(req, res, next) {
+  res.render('france_avc', { title: 'Express' });
+});
+
+router.get('/temoignages', function(req, res, next) {
+  res.render('temoignages', { title: 'Express' });
+});
+
+router.get('/info', function(req, res, next) {
+  res.render('info', { title: 'Express' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
 });
 
 module.exports = router;
